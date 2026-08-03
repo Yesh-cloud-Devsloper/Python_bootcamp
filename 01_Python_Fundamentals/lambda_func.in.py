635d6626dd526ee27cc49f8a06ee5098fb34861e
Yesh-cloud-Devsloper <yasabantakumarjspofficial@gmail.com>
2026-08-03 23:41:58 +0530

    Removed duplicate Day1 folder and updated lambda function example

--- 01_Python_Fundamentals/lambda_func.in.py
+++ 01_Python_Fundamentals/lambda_func.in.py
@@ -1,6 +1,7 @@
-Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> #find sum of only even numbers
+# Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+# Enter "help" below or click "Help" above for more information.
+# >>> 
+#find sum of only even numbers
 numbers=[20,21,22,23,12,16,40]
 even=list(filter(lambda x:x%2==0))
 sum_even=reduce(lambda x,y:x+y,even)
